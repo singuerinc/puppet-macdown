4da8b73b217f97aa70cadc4913ac9930938d25f8
Nahuel Scotti <nahuel.scotti@gmail.com>
2014-08-28 00:25:15 +0200

    initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Installs MacDown. The open source Markdown editor for OS X.
+#
+# Usage:
+#
+#  include macdown
+#
+class macdown() {
+  package { 'Macdown':
+    provider => 'compressed_app',
+    source   => 'http://macdown.uranusjr.com/download/latest/'
+  }
 }
